b705e42a71f266f66946fb12e75803765ccab4f6
Stevy91 <stevesainthubert701@gmail.com>
2025-08-25 14:21:36 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"; 
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
@@ -13,16 +13,13 @@ export default defineConfig({
     port: 3000,
     host: "0.0.0.0",
     proxy: process.env.NODE_ENV === "development" ? {
-  "/api": {
-    target: "https://steve-airways-production.up.railway.app",
-    changeOrigin: true,
-    secure: false,
-    // ne pas réécrire le chemin si ton backend a les routes avec /api
-    // rewrite: (path) => path.replace(/^\/api/, ""),
-    ws: true,
-  },
-} : undefined,
-
+      "/api": {
+        target: "https://steve-airways-production.up.railway.app",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    } : undefined,
   },
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
