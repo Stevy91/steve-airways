087b783ac01ec27f3ea5bf8fce84cdcebbd833b0
stevy9195 <steveshs701@gmail.com>
2026-01-03 12:11:55 -0500

    Description courte

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5693,52 +5693,9 @@ app.get("/api/flight-plane-search", async (req: Request, res: Response) => {
   }
 });
 
+
+
 app.get("/api/passengers/search", async (req: Request, res: Response) => {
-  try {
-    const { q } = req.query;
-
-    if (!q || String(q).length < 2) {
-      return res.json([]);
-    }
-
-    const [rows] = await pool.query<mysql.RowDataPacket[]>(
-      `
-      SELECT 
-        id,
-        first_name,
-        middle_name,
-        last_name,
-        date_of_birth,
-        idClient,
-        idTypeClient,
-        gender,
-        title,
-        address,
-        country,
-        nationality,
-        phone,
-        email,
-        nom_urgence,
-        email_urgence,
-        tel_urgence
-      FROM passengers
-      WHERE 
-        first_name LIKE ? 
-        OR last_name LIKE ?
-      ORDER BY created_at DESC
-      LIMIT 10
-      `,
-      [`%${q}%`, `%${q}%`]
-    );
-
-    res.json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-app.get("/api/passengers/search2", async (req: Request, res: Response) => {
   try {
     const { q } = req.query;
 
